19f3bd8ceb27afc070bbdab121c6454f56e8e963
steve smodish <smodish@hotmail.com>
2023-05-16 12:38:41 -0400

    scaffold items for project3

--- project3/src/App.tsx
+++ project3/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import Form from "./components/Form";
+import Footer from "./components/Footer";
+
+import "./App.css";
+
+interface ICardData {
+  cardNumber: string;
+  expirationDate: string;
+  cvv: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cardData, setCardData] = useState({});
+
+  const handleChange = (newCardData: ICardData) => {
+    setCardData({ ...newCardData });
+  };
+
+  console.log(cardData);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Card cardData={cardData} />
+      <Form cardData={cardData} handleChange={handleChange} />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
