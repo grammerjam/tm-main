45b23cd22d537abbb8a0a2585ca32d8e7a431e9f
alexvcs <alexcurtisslep@gmail.com>
2023-05-17 11:46:11 -0400

    manipulating data

--- project3/src/components/Card.tsx
+++ project3/src/components/Card.tsx
@@ -1,5 +1,10 @@
-const Card = () => {
-  return <div className="text-4xl">Card</div>;
+const Card = ({ formData }) => {
+  return (
+    <div className="text-4xl">
+      Card
+      <div>{formData.expirationMonth}</div>
+    </div>
+  );
 };
 
 export default Card;
