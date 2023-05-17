fbbc2df214c25b6ee9f6cefad3d3b695c472aeae
steve smodish <smodish@hotmail.com>
2023-05-17 14:46:44 -0400

    restored validation regex

---
+++ project3/src/utils/validation.js
@@ -0,0 +1,10 @@
+export const nameRegex = /^[a-zA-Z\s]+$/;
+
+export const cardCheck =
+  /^(1298|1267|4512|4567|8901|8933)([\-\s]?[0-9]{4}){3}$/;
+
+export const monthCheck = /^(0[1-9]|1[0-2])$/;
+
+export const yearCheck = /^20[0-9]{2}$/;
+
+export const cvcCheck = /^\d{3}$/;
