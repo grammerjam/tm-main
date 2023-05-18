47cbaff408d85ec4be28e5c7c0768b9d2ea669ad
steve smodish <smodish@hotmail.com>
2023-05-18 16:20:06 -0400

    validates with default form styles

--- project3/src/components/Form.jsx
+++ project3/src/components/Form.jsx
@@ -1,3 +1,11 @@
+import {
+  nameCheck,
+  cardCheck,
+  monthCheck,
+  yearCheck,
+  cvcCheck,
+} from "../utils/validation";
+
 const Form = ({ formData, onSubmit, handleChange }) => {
   return (
     <form onSubmit={onSubmit}>
@@ -8,6 +16,7 @@ const Form = ({ formData, onSubmit, handleChange }) => {
         required
         value={formData.cardholderName}
         placeholder="e.g. Jane Appleseed"
+        pattern="^[a-zA-Z\s]+$"
       />
       <input
         name="cardNumber"
@@ -16,6 +25,7 @@ const Form = ({ formData, onSubmit, handleChange }) => {
         value={formData.cardNumber}
         required
         placeholder="e.g. 1234 5678 9123 0000"
+        pattern="^(1298|1267|4512|4567|8901|8933)([\-\s]?[0-9]{4}){3}$"
       />
       <input
         name="expirationMonth"
@@ -24,6 +34,7 @@ const Form = ({ formData, onSubmit, handleChange }) => {
         required
         value={formData.expirationMonth}
         placeholder="MM"
+        pattern="^(0[1-9]|1[0-2])$"
       />
       <input
         name="expirationYear"
@@ -32,6 +43,7 @@ const Form = ({ formData, onSubmit, handleChange }) => {
         required
         value={formData.expirationYear}
         placeholder="YY"
+        pattern="^[0-9]{2}$"
       />
       <input
         name="cvc"
@@ -40,6 +52,7 @@ const Form = ({ formData, onSubmit, handleChange }) => {
         required
         value={formData.cvc}
         placeholder="e.g. 123"
+        pattern="^\d{3}$"
       />
 
       <input type="submit" />
