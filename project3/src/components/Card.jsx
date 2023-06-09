115bccffb19e7770320c7ade215ed8f0ea93590e
steve smodish <smodish@hotmail.com>
2023-06-09 12:43:14 -0400

    Mobile View - card faces laid out

--- project3/src/components/Card.jsx
+++ project3/src/components/Card.jsx
@@ -3,16 +3,16 @@ import cardFront from "/assets/bg-card-front.png";
 
 const Card = ({ formData }) => {
   return (
-    <div className="text-white">
-      <div className="obverse">
-        <img src={cardFront} alt="card front" className="w-64 h-auto" />
+    <div className="text-white w-full grid columns-1 grid-rows-6 pt-8 px-4">
+      <div className="obverse z-10 col-start-1 row-start-3 row-end-7">
+        <img src={cardFront} alt="card front" className="w-[280px] h-auto" />
         <div>{formData.cardNumber}</div>
         <div>{formData.cardholderName}</div>
         <div>{formData.expirationMonth}</div>
         <div>{formData.expirationYear}</div>
       </div>
-      <div className="inverse">
-        <img src={cardBack} alt="card back" className="w-64 h-auto" />
+      <div className="inverse col-start-1 row-start-1 row-end-4 justify-self-end">
+        <img src={cardBack} alt="card back" className="w-[280px] h-auto" />
         <div>{formData.cvc}</div>
       </div>
     </div>
