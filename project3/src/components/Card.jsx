c054da4ae25103142a4356e65f09738d335ca3ef
steve smodish <smodish@hotmail.com>
2023-06-09 13:54:21 -0400

    unfilled form data overlays

--- project3/src/components/Card.jsx
+++ project3/src/components/Card.jsx
@@ -2,18 +2,40 @@ import cardBack from "/assets/bg-card-back.png";
 import cardFront from "/assets/bg-card-front.png";
 
 const Card = ({ formData }) => {
+  const displayNumber =
+    formData.cardNumber === "" ? "0000000000000000" : formData.cardNumber;
+  const displayName =
+    formData.cardholderName === "" ? "Jane Appleseed" : formData.cardholderName;
+  const displayMonth =
+    formData.expirationMonth === "" ? "00" : formData.expirationMonth;
+  const displayYear =
+    formData.expirationYear === "" ? "00" : formData.expirationYear;
+  const displayCode = formData.cvc === "" ? "000" : formData.cvc;
+
   return (
     <div className="text-white w-full grid columns-1 grid-rows-6 pt-8 px-4">
-      <div className="obverse z-10 col-start-1 row-start-3 row-end-7">
-        <img src={cardFront} alt="card front" className="w-[280px] h-auto" />
-        <div>{formData.cardNumber}</div>
-        <div>{formData.cardholderName}</div>
-        <div>{formData.expirationMonth}</div>
-        <div>{formData.expirationYear}</div>
+      <div className="obverse z-10 col-start-1 row-start-3 row-end-7 grid columns-1">
+        <img
+          src={cardFront}
+          alt="card front"
+          className="w-[280px] h-auto col-start-1 row-start-1"
+        />
+        <div className="col-start-1 row-start-1">
+          <div>{displayNumber}</div>
+          <div>{displayName}</div>
+          <div>{displayMonth}</div>
+          <div>{displayYear}</div>
+        </div>
       </div>
-      <div className="inverse col-start-1 row-start-1 row-end-4 justify-self-end">
-        <img src={cardBack} alt="card back" className="w-[280px] h-auto" />
-        <div>{formData.cvc}</div>
+      <div className="inverse col-start-1 row-start-1 row-end-4 justify-self-end grid columns-1">
+        <img
+          src={cardBack}
+          alt="card back"
+          className="w-[280px] h-auto col-start-1 row-start-1"
+        />
+        <div className="col-start-1 row-start-1">
+          <div>{displayCode}</div>
+        </div>
       </div>
     </div>
   );
