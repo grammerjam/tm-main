ae0d243e17dfb0bd9e02ca9852205ddc671a2006
steve smodish <smodish@hotmail.com>
2023-07-28 11:45:20 -0400

    update server ssl connection

--- project3D/server/config/db.js
+++ project3D/server/config/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_CONNECT);
+    const conn = await mongoose.connect(process.env.DB_CONNECT, {
+      ssl: true,
+      sslCA: `${__dirname}/global-bundle.pem`,
+      tlsCAFile: `global-bundle.pem`,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
